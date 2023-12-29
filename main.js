ef82f0ad3404983711be151d69629035b9e9f5ff
probably-kit <probably.kit@gmail.com>
2023-12-29 16:27:13 +0100

    refactored js code

--- main.js
+++ main.js
@@ -1,86 +1,77 @@
-const board = document.getElementById('gameBoard');
-const restartButton = document.getElementById('restartButton');
-const xCounter = document.getElementById('xCounter');
-const oCounter = document.getElementById('oCounter');
-//put in the object
-let currentPlayer = 'X';
-let gameBoard = ['', '', '', '', '', '', '', '', ''];
-let gameActive = true;
-let xWins = 0;
-let oWins = 0;
+class TicTacToe {
+    constructor() {
+        this.board = document.getElementById('gameBoard');
+        this.restartButton = document.getElementById('restartButton');
+        this.xCounter = document.getElementById('xCounter');
+        this.oCounter = document.getElementById('oCounter');
+        this.currentPlayer = 'X';
+        this.gameBoard = Array(9).fill('');
+        this.gameActive = true;
+        this.xWins = 0;
+        this.oWins = 0;
 
-// Initialize the game board
-function initializeBoard() {
-    for (let i = 0; i < 9; i++) {
-        const cell = document.createElement('div');
-        cell.className = 'cell';
-        cell.setAttribute('data-index', i);
-        cell.addEventListener('click', handleCellClick);
-        board.appendChild(cell);
+        this.initializeBoard();
+        this.restartButton.addEventListener('click', () => this.restartGame());
     }
-    restartButton.addEventListener('click', restartGame);
-    updateCounters();
-}
 
-// Handle cell click event
-function handleCellClick(event) {
-    const index = event.target.getAttribute('data-index');
+    initializeBoard() {
+        this.board.innerHTML = '';
+        this.board.addEventListener('click', event => this.handleCellClick(event));
+        for (let i = 0; i < 9; i++) {
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.dataset.index = i;
+            this.board.appendChild(cell);
+        }
+        this.updateCounters();
+    }
 
-    // Check if the cell is already occupied or the game is over
-    if (!gameBoard[index] && gameActive) {
-        gameBoard[index] = currentPlayer;
-        event.target.textContent = currentPlayer;
+    handleCellClick(event) {
+        const cell = event.target;
+        const index = cell.dataset.index;
 
-        if (checkWinner()) {
-            alert(`Player ${currentPlayer} wins!`);
-            currentPlayer === 'X' ? xWins++ : oWins++;
-            updateCounters();
-            gameActive = false;
-        } else if (gameBoard.every(cell => cell !== '')) {
-            alert('It\'s a draw!');
-            gameActive = false;
-        } else {
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        if (cell.className.includes('cell') && !this.gameBoard[index] && this.gameActive) {
+            this.gameBoard[index] = this.currentPlayer;
+            cell.textContent = this.currentPlayer;
+
+            if (this.checkWinner()) {
+                alert(`Player ${this.currentPlayer} wins!`);
+                this.currentPlayer === 'X' ? this.xWins++ : this.oWins++;
+                this.updateCounters();
+                this.gameActive = false;
+            } else if (this.gameBoard.every(cell => cell !== '')) {
+                alert('It\'s a draw!');
+                this.gameActive = false;
+            } else {
+                this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+            }
         }
     }
+
+    checkWinner() {
+        const winPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+
+        return winPatterns.some(pattern => {
+            const [a, b, c] = pattern;
+            return this.gameBoard[a] && this.gameBoard[a] === this.gameBoard[b] && this.gameBoard[a] === this.gameBoard[c];
+        });
+    }
+
+    restartGame() {
+        this.gameBoard.fill('');
+        this.gameActive = true;
+        this.currentPlayer = 'X';
+        this.initializeBoard();
+    }
+
+    updateCounters() {
+        this.xCounter.textContent = `X Wins: ${this.xWins}`;
+        this.oCounter.textContent = `O Wins: ${this.oWins}`;
+    }
 }
 
-// Check for a winner
-function checkWinner() {
-    const winPatterns = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-
-    return winPatterns.some(pattern => {
-        const [a, b, c] = pattern;
-        return gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c];
-    });
-}
-
-// Restart the game
-function restartGame() {
-    // Clear the game board
-    board.innerHTML = '';
-    gameBoard = Array(9).fill('');
-    gameActive = true;
-    currentPlayer = 'X';
-
-    // Re-initialize the game board
-    initializeBoard();
-}
-
-// Update X and O counters
-function updateCounters() {
-    xCounter.textContent = `X Wins: ${xWins}`;
-    oCounter.textContent = `O Wins: ${oWins}`;
-}
-
-// Start the game
-initializeBoard();
+new TicTacToe();
